aadf49da4b933b9fca398ca4d94d6e3bfd0b19ac
Martin Idel <martin.idel@tngtech.com>
2024-10-30 15:14:10 +0100

    Add examples how to use the API in a go project
    Signed-off-by: Martin Idel <martin.idel@tngtech.com>

---
+++ examples/main.go
@@ -0,0 +1,31 @@
+// Copyright (c) Outernet Council and Contributors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+
+	"google.golang.org/protobuf/proto"
+	"oc.com/smo/api/fed/v1alpha"
+)
+
+func main() {
+	e := v1alpha.CancelServiceRequest{
+		ServiceId: "service-id",
+	}
+
+	buf, _ := proto.Marshal(&e)
+	os.WriteFile("federation.protobuf", buf, os.ModePerm)
+}
